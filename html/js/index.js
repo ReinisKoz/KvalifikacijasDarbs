029fc1ece53601f12bd3a8bc19131f22a99a4126
Reinis <rk00345@rvt.lv>
2024-10-03 12:37:47 +0300

    sidebar

--- html/js/index.js
+++ html/js/index.js
@@ -1,3 +1,11 @@
 document.querySelector('#dark-mode-toggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 });
+
+// Sidebar
+const menuButton = document.getElementById('menuButton');
+const Sidebar = document.getElementById('sidebar');
+
+menuButton.addEventListener('click', function() {
+    Sidebar.classList.toggle('active');
+});
