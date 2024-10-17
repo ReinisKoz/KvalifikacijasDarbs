3849791fc352c7b7601708055093ea087736b275
ReinisKoz <127198110+ReinisKoz@users.noreply.github.com>
2024-10-17 09:47:58 +0000

    Nedaudz pievienoju kaut ko jaunu

--- html/js/index.js
+++ html/js/index.js
@@ -7,3 +7,26 @@ document.getElementById('menuButton').addEventListener('click', function() {
 document.getElementById('dark-mode-toggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 });
+
+function changeLanguage(lang) {
+    const elements = document.querySelectorAll("[data-lang]");
+    elements.forEach(element => {
+        element.textContent = translations[lang][element.getAttribute("data-lang")];
+    });
+}
+
+// Translations for different languages
+const translations = {
+    "en": {
+        "title": "Welcome to our website!",
+        "contact": "Contact us"
+    },
+    "fr": {
+        "title": "Bienvenue sur notre site Web!",
+        "contact": "Contactez-nous"
+    },
+    "lv": {
+        "title": "Laipni lūdzam mūsu mājaslapā!",
+        "contact": "Sazinieties ar mums"
+    }
+};
